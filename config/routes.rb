c8f408d4cef8cc4ee3a776123ff2fdec159d17aa
KentaToya <163114083+kenta1129@users.noreply.github.com>
2024-04-19 12:11:43 +0000

    [課題３]top]　

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   
-  get 'users/show'
-  get 'users/edit'
-  root to: "homes#top"
+  root to: 'homes#top'
+  get 'homes/about', to: 'homes#about', as: :about
   devise_for :users
   
   resources :books, only: [:new, :index, :show, :edit, :update]
